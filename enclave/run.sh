1d726637f0ffc6e3604c165a28e679e3f3838d27
0xktn <0xktn@users.noreply.github.com>
2025-12-13 01:02:05 +0700

    chore(debug): deploy minimal bash hello world to test enclave stability

--- enclave/run.sh
+++ enclave/run.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
-
-# Log everything to console
-exec 2>&1
-
-echo "[ENCLAVE] Starting Python app..." > /dev/console
-
-# Setup Python environment
-cd /app
-source venv/bin/activate
-
-# Run app
-python3 -u app.py > /dev/console 2>&1
-
-# Loop forever if app crashes
-while true; do sleep 60; done
+while true; do
+  echo "Hello Enclave" > /dev/console
+  sleep 5
+done
